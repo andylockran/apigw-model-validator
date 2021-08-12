e2de812aa6ac6c121fd8b4e8595abaae90912a6f
Andrew Loughran <andy@zrmt.com>
2021-08-12 22:50:30 +0100

    Updated tests for negative test

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,8 +5,14 @@ import cmd = require('../src')
 describe('apigw-model-validator', () => {
   test
   .stdout()
-  .do(() => cmd.run(['-s', 'schema/example.yaml', '--model', 'Pet', 'payload.json']))
-  .it('runs -s schema/example.yaml -m Pet payload.json', ctx => {
+  .do(() => cmd.run(['-s', 'schema/example.yaml', '--model', 'Pet', 'examples/good-payload.json']))
+  .it('runs -s schema/example.yaml -m Pet examples/good-payload.json', ctx => {
     expect(ctx.stdout).to.contain('Payload passed validation')
+  })
+  test
+  .stdout()
+  .do(() => cmd.run(['-s', 'schema/example.yaml', '--model', 'Pet', 'examples/bad-payload.json']))
+  .it('runs -s schema/example.yaml -m Pet examples/bad-payload.json', ctx => {
+    expect(ctx.stdout).to.contain('integer')
   })
 })
