5a80a410c5588fc5d33fdf59505e20758b8315f0
Andrew Loughran <andy@zrmt.com>
2021-08-21 11:44:21 +0100

    Updated to allow for path/method lookups too

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import addFormats from 'ajv-formats'
 import {readFileSync} from 'fs'
 import path from 'path'
 import {load} from 'js-yaml'
+import fs from 'fs'
 
 class ApigwModelValidator extends Command {
   static description = 'Validate your json object against an OpenAPI definition'
@@ -16,6 +17,10 @@ class ApigwModelValidator extends Command {
     schema: flags.string({char: 's', description: 'path to openapi definition'}),
     // flag with a value (-m, --model=VALUE)
     model: flags.string({char: 'm', description: 'model to validate your payload against'}),
+    // flag with a value (-p, --path=VALUE)
+    path: flags.string({char: 'p', description: 'The API path to validate against (if you do not know the model)'}),
+    // flag with a value (-r, --requestMethod=VALUE)
+    requestMethod: flags.string({char: 'r', description: 'The request method GET/POST to tests'}),
   }
 
   static args = [{name: 'payload'}]
@@ -36,22 +41,32 @@ class ApigwModelValidator extends Command {
     const {args, flags} = this.parse(ApigwModelValidator)
     if (flags.schema) this.log(`--schema is: ${flags.schema}`)
     if (flags.model) this.log(`--model is: ${flags.model}`)
+    if (flags.path) this.log(`--path is: ${flags.path}`)
 
-    await this.loadOpenAPIDefs(flags.schema)
+    if (flags.path && flags.model) {
+      this.error('Please only provide either path or model', {exit: 1})
+    }
+
+    await this.loadOpenAPISchema(flags.schema)
+
     await this.createValidator(flags.model)
     await this.readPayload(args.payload)
-    // console.log(this.ajv.schemas)
-    // this.log(this.validator)
-    const answer = this.validator(this.payload)
-    if (answer) {
-      this.log('Payload passed validation')
-    } else {
-      this.log(this.validator.errors)
+
+    if (flags.model) {
+      this.validateModel(flags.model)
+    }
+    if (flags.path) {
+      this.validatePath(flags.path, flags.requestMethod)
     }
   }
 
-  async loadOpenAPIDefs(schema: any) {
-    this.openapi = load(readFileSync(path.resolve(process.cwd(), schema), 'utf8'))
+  async loadOpenAPISchema(schema: any) {
+    const openapifile: string = path.resolve(process.cwd(), schema)
+    if (fs.existsSync(openapifile)) {
+      this.openapi = load(readFileSync(openapifile, 'utf8'))
+    } else {
+      this.error('The OpenAPI file does not exist.', {exit: 1})
+    }
   }
 
   async createValidator(model: any) {
@@ -59,17 +74,43 @@ class ApigwModelValidator extends Command {
     addFormats(this.ajv)
     const schemaList = this.openapi.components.schemas
     for (const property in schemaList) {
-      // this.log(`${property}: ${schemaList[property]}`)
-      const schema_model = schemaList[property]
-      schema_model.id = `https://example.com/schema.json#/components/schemas/${property}`
-      this.ajv.addSchema(schemaList[property])
+      if (Object.prototype.hasOwnProperty.call(schemaList, property)) {
+        const schema_model = schemaList[property]
+        schema_model.id = `https://example.com/schema.json#/components/schemas/${property}`
+        this.ajv.addSchema(schemaList[property])
+      }
     }
-
-    this.validator = this.ajv.compile(schemaList[model])
   }
 
   async readPayload(payload: any) {
-    this.payload = load(readFileSync(path.resolve(process.cwd(), payload), 'utf8'))
+    const payloadfile = path.resolve(process.cwd(), payload)
+    if (fs.existsSync(payloadfile)) {
+      this.payload = load(readFileSync(payloadfile, 'utf8'))
+    } else {
+      this.error('Your payload file does not exist.', {exit: 1})
+    }
+  }
+
+  async validateModel(model: any) {
+    const schemaList = this.openapi.components.schemas
+    this.validator = this.ajv.compile(schemaList[model])
+
+    const answer = this.validator(this.payload)
+
+    if (answer) {
+      this.log('Payload passed validation')
+    } else {
+      this.log(this.validator.errors)
+    }
+  }
+
+  async validatePath(path: any, requestMethod: any) {
+    const urlPath = this.openapi.paths[path]
+    const method = requestMethod.toLowerCase()
+    const schemaDef = urlPath[method].requestBody.content['application/json'].schema['$ref'].split("/")
+    const model = schemaDef[schemaDef.length - 1]
+    this.validateModel(model)
+    return true
   }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -15,4 +15,19 @@ describe('apigw-model-validator', () => {
   .it('runs -s schema/example.yaml -m Pet examples/bad-payload.json', ctx => {
     expect(ctx.stdout).to.contain('integer')
   })
+  test
+  .stderr()
+  .do(() => cmd.run(['-s', 'schema/example.yaml', '--model', 'Pet', 'examples/does-not-exist-payload.json']))
+  .exit(1)
+  .it('exits with status 1 if payload is not provided')
+  test
+  .stdout()
+  .do(() => cmd.run(['-s', 'schema/example.yaml', '--model', 'Pet', '--path', '/test', 'examples/bad-payload.json']))
+  .exit(1)
+  .it('exits with status 1 if both path and model are set')
+  test
+  .stdout()
+  .do(() => cmd.run(['-s', 'schema/not-exist.yaml', '--model', 'Pet', 'examples/bad-payload.json']))
+  .exit(1)
+  .it('exits with status 1 if the schema does not exist')
 })
